17b8171812d3d12bc6b954c11c754a3c20800489
waaake <vivek_ve@outlook.com>
2025-05-27 23:02:16 +0530

    [VOID] Added FrameCache for Track & TrackItem
    Frame cache emits signal to allow cache to be represented in the timeslider

---
+++ src/VoidUi/ControlBar.h
@@ -0,0 +1,83 @@
+#ifndef _VOID_VIEWER_CONTROL_BAR_H
+#define _VOID_VIEWER_CONTROL_BAR_H
+
+/* Qt */
+#include <QLayout>
+#include <QPushButton>
+#include <QSlider>
+#include <QWidget>
+
+/* Internal */
+#include "Definition.h"
+#include "VoidCore/Logging.h"
+
+/* Describes the amount of zoom (in or out) that happens when the zoom buttons are pressed */
+static const int ZOOM_STEP = 10;
+
+VOID_NAMESPACE_OPEN
+
+class ControlSlider : public QSlider
+{
+    Q_OBJECT
+
+public:
+    ControlSlider(Qt::Orientation orientation, QWidget* parent = nullptr);
+    virtual ~ControlSlider();
+
+protected:
+    void paintEvent(QPaintEvent* event) override;
+
+};
+
+class ControlBar : public QWidget
+{
+    Q_OBJECT
+
+public:
+    ControlBar(QWidget* parent = nullptr);
+    virtual ~ControlBar();
+
+    /*
+     * Since the viewport could also have a zoom applied on itself, that value should then also
+     * need to reflect on the zoom slider, and hence the zoom factor needs to be translated to Slider
+     * space and then set on the Zoom slider
+     * Also since the slider is connected to emit zoomChanged with value changes,
+     * the signals are blocked till the time the value has been set on the slider and unblocked after
+     */
+    void SetFromZoom(float zoom);
+
+signals:
+    void zoomChanged(float factor);
+
+private: /* Members */
+    /* Base Layout */
+    QHBoxLayout* m_Layout;
+
+    /* Zoom Controls */
+    QPushButton* m_ZoomInButton;
+    QPushButton* m_ZoomOutButton;
+    ControlSlider* m_ZoomSlider;
+
+private: /* Methods */
+    void Build();
+    void Setup();
+    void Connect();
+
+    /* Zoom Updates */
+    inline void ZoomIn() { m_ZoomSlider->setValue(m_ZoomSlider->value() + ZOOM_STEP); }
+    inline void ZoomOut() { m_ZoomSlider->setValue(m_ZoomSlider->value() - ZOOM_STEP); }
+
+    /* 
+     * Emits the zoomChanged signal with the value from zoom slider normalized to viewport zoom
+     */
+    inline void UpdateZoom(int value) { emit zoomChanged(MapToZoom(value)); }
+    // inline void UpdateZoom(int zoom) { VOID_LOG_INFO("Z: {0}", MapToZoom(zoom)); }
+
+    float MapToZoom(int value);
+    int MapFromZoom(float zoom);
+
+};
+
+VOID_NAMESPACE_CLOSE
+
+#endif // _VOID_VIEWER_CONTROL_BAR_H
