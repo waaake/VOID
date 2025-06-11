f427ebafbf349c8d77c71cc31f02cb50a89b3a48
waaake <vivek_ve@outlook.com>
2025-06-11 19:21:50 +0530

    [VOID] VoidUniqueIdentifier added: Vuid is a way to distinguish between component instances
    All Identifiable instances derive from VoidObject.

---
+++ src/VoidCore/Identity.h
@@ -0,0 +1,45 @@
+#ifndef _VOID_UNIQUE_IDENTITY_H
+#define _VOID_UNIQUE_IDENTITY_H
+
+/* Boost */
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+
+/* Internal */
+#include "Definition.h"
+
+VOID_NAMESPACE_OPEN
+
+/**
+ * Describes a Unique Identity for Void Component Instances
+ * This is primarily used across MediaClip, Sequence and Track instances
+ * Making them distinguishable from the other entities
+ */
+class VUID
+{
+public:
+    /* Generate a unique identifier */
+    VUID() : m_uuid(m_gen()) {}
+
+    /* Equatation */
+    bool operator==(const VUID& other) const { return m_uuid == other.m_uuid; }
+
+    /* For Representation */
+    inline std::string str() const { return boost::uuids::to_string(m_uuid); }
+
+private: /* Members */
+    boost::uuids::random_generator m_gen;
+    boost::uuids::uuid m_uuid;
+};
+
+// std::ostream& operator<<(std::ostream& stream, const VUID& v)
+// {
+//     /* Add a VUID identifier */
+//     stream << "VUID{"<< v.str() <<"}";
+//     return stream;
+// }
+
+VOID_NAMESPACE_CLOSE
+
+#endif // _VOID_UNIQUE_IDENTITY_H
---
+++ src/VoidUi/VoidObject.h
@@ -0,0 +1,32 @@
+#ifndef _VOID_OBJECT_H
+#define _VOID_OBJECT_H
+
+/* Qt*/
+#include <QObject>
+
+/* Internal */
+#include "Definition.h"
+#include "VoidCore/Identity.h"
+
+VOID_NAMESPACE_OPEN
+
+class VoidObject : public QObject
+{
+public:
+    VoidObject(QObject* parent = nullptr) : QObject(parent), m_Vuid() {}
+
+    /* Accessible Void Unique Identifier in a string representation */
+    inline std::string Vuid() const
+    { 
+        std::string result = "VUID{";
+        result.append(m_Vuid.str()).append("}");
+        return result;
+    }
+
+private: /* Members */
+    VUID m_Vuid;
+};
+
+VOID_NAMESPACE_CLOSE
+
+#endif // _VOID_OBJECT_H
