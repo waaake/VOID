a8a3f2c29caa4405128dea30a6a1b1c0d36eb7c0
waaake <vivek_ve@outlook.com>
2025-08-28 17:40:19 +0530

    [VOID] CMake: FindTurboJPEG cmake for windows includes the DLL file to be copied onto the bin directory

--- cmake/Modules/FindTurboJPEG.cmake
+++ cmake/Modules/FindTurboJPEG.cmake
@@ -9,3 +9,14 @@ if (TurboJPEG_FOUND)
     set(TurboJPEG_LIBRARIES ${TurboJPEG_LIBRARY})
     set(TurboJPEG_INCLUDE_DIRS ${TurboJPEG_INCLUDE_DIR})
 endif()
+
+if (WIN32 AND TurboJPEG_FOUND)
+    get_filename_component(TurboJPEG_LIB_DIR ${TurboJPEG_LIBRARY} DIRECTORY) # ..
+    get_filename_component(TurboJPEG_DIR ${TurboJPEG_LIB_DIR} DIRECTORY) # ..
+    
+    # Bin Directory
+    set(TurboJPEG_BIN_DIR "${TurboJPEG_DIR}/bin")
+
+    # Find turbojpeg.dll from the bin directory
+    file(GLOB TurboJPEG_DLL "${TurboJPEG_BIN_DIR}/turbojpeg*.dll")
+endif()
