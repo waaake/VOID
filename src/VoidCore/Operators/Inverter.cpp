a34c517cb04cc3e5dfbdb44fc3df10841deef0fa
waaake <vivek_ve@outlook.com>
2026-04-14 19:58:13 +0530

    [VOID] Operator: Updated inverter to only invert RGB channels

--- src/VoidCore/Operators/Inverter.cpp
+++ src/VoidCore/Operators/Inverter.cpp
@@ -9,10 +9,18 @@ VOID_NAMESPACE_OPEN
 
 bool InvertOp::Evaluate(ImageRow& row)
 {
+    if (row.channels < 3)
+        return false;
+
     for (std::size_t i = 0; i < row.width; ++i)
     {
         unsigned char* pixel = row.Pixel<unsigned char>(i);
-        for (std::size_t channel = 0; channel < row.channels; ++channel)
+
+        // Skip the alpha channel inversion, it causes issues at the moment with exr based data
+        // If we go back to using exrs as float data, then it might work fine, need to check that
+        // exr with float are very heavy as well (4 byte per channel vs 1 byte -- 8 bits for unsigned char), 
+        // need to check that as well for later
+        for (std::size_t channel = 0; channel < 3; ++channel)
             pixel[channel] = 255 - pixel[channel];
     }
 
