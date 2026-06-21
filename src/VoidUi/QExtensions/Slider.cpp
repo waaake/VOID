e87562273e70733067f2ca32d222508d3b1f2ae6
waaake <vivek_ve@outlook.com>
2026-06-21 11:20:53 +0530

    [VOID] Slider: Added quick double slider

--- src/VoidUi/QExtensions/Slider.cpp
+++ src/VoidUi/QExtensions/Slider.cpp
@@ -1,10 +1,15 @@
 // Copyright (c) 2025 waaake
 // Licensed under the MIT License
 
+/* Qt */
+#include <QDoubleValidator>
+
 /* Internal */
 #include "Slider.h"
 
 VOID_NAMESPACE_OPEN
+
+/// Frameless Slider
 
 FramelessSlider::FramelessSlider(QWidget* parent)
     : QWidget(parent, Qt::Popup | Qt::FramelessWindowHint)
@@ -29,6 +34,97 @@ void FramelessSlider::Build()
     m_Slider = new QSlider(Qt::Horizontal, this);
 
     m_Layout->addWidget(m_Slider);
+}
+
+/// QuickDoubleSlider
+
+QuickDoubleSlider::QuickDoubleSlider(QWidget* parent)
+    : QWidget(parent)
+    , m_Min(0)
+    , m_Max(100)
+    , m_Factor(100)
+{
+    Build();
+    Setup();
+}
+
+QuickDoubleSlider::~QuickDoubleSlider()
+{
+    m_Layout->deleteLater();
+    delete m_Layout;
+    m_Layout = nullptr;
+}
+
+void QuickDoubleSlider::SetValue(double value)
+{
+    m_Slider->setValue(value * m_Factor);
+}
+
+void QuickDoubleSlider::SetMinimum(double min)
+{
+    if (min == 0)
+    {
+        // m_Factor = 100;
+        m_Slider->setMinimum(0);
+    }
+    else if (min < 0)
+    {
+        // m_Factor = std::abs(1 / min);
+        m_Slider->setMinimum(min * m_Factor);
+    }
+    else
+    {
+        // m_Factor = 1 / min;
+        m_Slider->setMinimum(1);
+    }
+}
+
+void QuickDoubleSlider::SetMaximum(double max)
+{
+    m_Slider->setMaximum(max * m_Factor);
+}
+
+void QuickDoubleSlider::SetRange(double min, double max)
+{
+    SetMinimum(min);
+    SetMaximum(max);
+}
+
+void QuickDoubleSlider::SetSingleStep(double step)
+{
+    m_Slider->setSingleStep(step * m_Factor);
+}
+
+void QuickDoubleSlider::Build()
+{
+    m_Layout = new QHBoxLayout(this);
+
+    m_Editor = new QLineEdit;
+    m_Slider = new QSlider(Qt::Horizontal);
+
+    m_Layout->addWidget(m_Editor);
+    m_Layout->addWidget(m_Slider);
+    m_Layout->setContentsMargins(0, 0, 0, 0);
+}
+
+void QuickDoubleSlider::Setup()
+{
+    QDoubleValidator* validator = new QDoubleValidator;
+    m_Editor->setMaximumWidth(80);
+    m_Editor->setValidator(validator);
+
+    // Default
+    m_Editor->setText(QString::number(static_cast<double>(m_Slider->value()) / m_Factor));
+
+    // Connections
+    connect(m_Slider, &QSlider::valueChanged, this, [this](int value) -> void
+    {
+        bool blocked = m_Editor->blockSignals(true);
+        m_Editor->setText(QString::number(static_cast<double>(value) / m_Factor));
+        m_Editor->blockSignals(blocked);
+
+        emit valueChanged(static_cast<double>(value) / m_Factor);
+    });
 }
 
 VOID_NAMESPACE_CLOSE
--- src/VoidUi/QExtensions/Slider.h
+++ src/VoidUi/QExtensions/Slider.h
@@ -6,6 +6,7 @@
 
 /* Qt */
 #include <QWidget>
+#include <QLineEdit>
 #include <QSlider>
 #include <QLayout>
 
@@ -38,7 +39,38 @@ private: /* Members */
 
 private: /* Methods */
     void Build();
+};
 
+class QuickDoubleSlider : public QWidget
+{
+    Q_OBJECT
+public:
+    QuickDoubleSlider(QWidget* parent = nullptr);
+    ~QuickDoubleSlider();
+
+    void SetValue(double value);
+    void SetMinimum(double min);
+    void SetMaximum(double max);
+    void SetRange(double min, double max);
+    void SetSingleStep(double step);
+
+    double Value() const { return m_Editor->text().toDouble(); }
+
+signals:
+    void valueChanged(double);
+
+private: /* Members */
+    QHBoxLayout* m_Layout;
+    QLineEdit* m_Editor;
+    QSlider* m_Slider;
+
+    int m_Min;
+    int m_Max;
+    unsigned int m_Factor;
+
+private: /* Methods */
+    void Build();
+    void Setup();
 };
 
 VOID_NAMESPACE_CLOSE
