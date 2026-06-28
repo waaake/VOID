027770fff7722c9bcba69f527b25c134b7ac3ee6
waaake <vivek_ve@outlook.com>
2026-06-28 13:39:53 +0530

    [VOID] Frame: Update Media Frame to work with the updated architecture for Media Reader

--- src/VoidCore/Media/Frame.h
+++ src/VoidCore/Media/Frame.h
@@ -27,51 +27,54 @@ public:
     Frame();
     explicit Frame(const MEntry& e);
     Frame(const MEntry& e, v_frame_t frame);
-
     ~Frame();
 
-    /* Move */
+    // Move
     Frame(Frame&& other) noexcept;
     Frame& operator=(Frame&& other) noexcept;
 
-    /* Copy */
+    // Copy
     Frame(const Frame& other);
     Frame& operator=(const Frame& other);
 
-    /* Getters */
-    inline const std::string& Path() const { return m_MediaEntry.Fullpath(); }
-    inline const std::string& Name() const { return m_MediaEntry.Name(); }
-    inline const std::string& Extension() const { return m_MediaEntry.Extension(); }
+    // Entry Attribs
+    inline const std::string& Path() const { return m_Entry.Fullpath(); }
+    inline const std::string& Name() const { return m_Entry.Name(); }
+    inline const std::string& Extension() const { return m_Entry.Extension(); }
+    
+    // Frame Attribs
     inline v_frame_t Framenumber() const { return m_Framenumber; }
     inline void SetDirty(bool dirty) { m_Dirty = dirty; }
-    [[nodiscard]] inline bool Valid() const { return bool(m_ImageData); }
-    [[nodiscard]] inline bool Invalid() const { return !m_ImageData; }
     [[nodiscard]] inline bool Dirty() const { return m_Dirty; }
-    [[nodiscard]] inline int Channels() const { return m_Channels; }
+    [[nodiscard]] inline bool Invalid() const { return !m_Reader; }
+    [[nodiscard]] inline bool Valid() const { return bool(m_Reader); }
 
-    /**
-     * Returns Shared Pointer to the ImageData
-     * cached defines whether the frame needs to be read before returning
-     * has no effect if the frame has already been read
-     */
-    SharedPixReader Image(bool cached = true);
-    SharedPixReader Writable();
+    // /**
+    //  * Returns Shared Pointer to the ImageData
+    //  * cached defines whether the frame needs to be read before returning
+    //  * has no effect if the frame has already been read
+    //  */
+    // SharedPixReader Image(bool cached = true);
+    // // SharedPixReader Writable();
+    void Thumbnail(UInt8Image& image);
+    void Image(FloatImage& image);
 
-    /**
-     * Returns the underlying metadata from the image
-     */
-    // inline const std::map<std::string, std::string> Metadata() const { return m_ImageData->Metadata(); }
+    // Image Properties
+    [[nodiscard]] inline int Width() const { return m_Reader->Width(); }
+    [[nodiscard]] inline int Height() const { return m_Reader->Height(); }
+    [[nodiscard]] inline int Channels() const { return m_Reader->Channels(); }
+    inline const std::map<std::string, std::string> Metadata() const { return m_Reader->Metadata(); }
 
     /* Frame Caches */
     void Cache();
     void ClearCache(bool dirty = true);
 
 protected: /* Members */
-    MEntry m_MediaEntry;
-    SharedPixReader m_ImageData;
-    SharedPixReader m_Writable;
+    MEntry m_Entry;
+    SharedPixReader m_Reader;
+    // SharedPixReader m_Writable;
     v_frame_t m_Framenumber;
-    int m_Channels = {0};
+    // int m_Channels = {0};
     bool m_Dirty = {false};
 
 private: /* Members*/
