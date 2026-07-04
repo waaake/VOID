a5a4a9956a7e37ef1fe8712317191ef3957c2bc0
waaake <vivek_ve@outlook.com>
2026-07-04 17:27:07 +0530

    [VOID] MediaLister: Dragging and dropping media directory now imports with progress bar in a separate thread

--- src/VoidUi/Media/MediaLister.cpp
+++ src/VoidUi/Media/MediaLister.cpp
@@ -106,7 +106,7 @@ void VoidMediaLister::dropEvent(QDropEvent* event)
             VOID_LOG_INFO("Dropped Media Directory: {0}", path);
 
             /* Emit the media dropped signal */
-            _MediaBridge.ImportDirectory(path, false);
+            _MediaBridge.ImportDirectory(path, true);
         }
     }
 }
