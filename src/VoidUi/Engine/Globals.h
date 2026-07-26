c94b766b26f20fcef185595bd104d32acc5e184d
waaake <vivek_ve@outlook.com>
2026-07-26 22:10:33 +0530

    [VOID] Binding: CoreModule::TrackItem gets timeline apis for setting/getting in-out values

--- src/VoidUi/Engine/Globals.h
+++ src/VoidUi/Engine/Globals.h
@@ -12,6 +12,7 @@
 #include "VoidObjects/Core/Task.h"
 #include "VoidUi/Media/MetadataViewer.h"
 #include "VoidUi/BaseWindow/MenuSystem.h"
+#include "VoidUi/Sequencer/Sequencer.h"
 #include "VoidUi/BaseWindow/PlayerWindow.h"
 
 VOID_NAMESPACE_OPEN
