1405ecc51b47ee041e102595c52152033111af74
waaake <vivek_ve@outlook.com>
2026-07-29 18:18:33 +0530

    [VOID] SequencerToolbar: Added Sequencer Tool action buttons in the toolbar allowing switching the action context for Sequencer

--- src/VoidUi/Sequencer/Widgets/SToolbar.h
+++ src/VoidUi/Sequencer/Widgets/SToolbar.h
@@ -5,12 +5,15 @@
 #define _SEQUENCER_TOOLBAR_H
 
 /* Qt */
+#include <QButtonGroup>
 #include <QFrame>
 #include <QLayout>
 #include <QToolButton>
 
 /* Internal */
 #include "Definition.h"
+#include "VoidUi/Sequencer/SContext.h"
+#include "VoidUi/QExtensions/PushButton.h"
 
 VOID_NAMESPACE_OPEN
 
@@ -22,14 +25,24 @@ public:
 
 signals:
     void reset();
+    void actionSwitched(const SequencerAction&);
 
 private:
     QVBoxLayout* m_Layout;
+    QButtonGroup m_ActionGroup;
+
     QToolButton* m_ResetButton;
+    HighlightToggleButton* m_PointerButton;
+    HighlightToggleButton* m_SlipClipButton;
+    HighlightToggleButton* m_RazorButton;
+    HighlightToggleButton* m_RazorAllButton;
+    HighlightToggleButton* m_MergeButton;
 
 private: /* Methods */
     void Build();
+    void Setup();
     void Connect();
+    void ActionSwitched(int action);
 };
 
 VOID_NAMESPACE_CLOSE
