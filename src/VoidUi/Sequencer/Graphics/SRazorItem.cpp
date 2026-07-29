c0e00edd403724f48841086c2f5594b81019797d
waaake <vivek_ve@outlook.com>
2026-07-29 18:44:06 +0530

    [VOID] RazorItem: Added RazorItem for drawing lines on SequenceView and Tracks

---
+++ src/VoidUi/Sequencer/Graphics/SRazorItem.cpp
@@ -0,0 +1,43 @@
+// Copyright (c) 2025 waaake
+// Licensed under the MIT License
+
+/* Qt */
+#include <QPainter>
+#include <QPalette>
+#include <QStyleOptionGraphicsItem>
+
+/* Internal */
+#include "SRazorItem.h"
+#include "VoidCore/Logging.h"
+
+VOID_NAMESPACE_OPEN
+
+SRazorItem::SRazorItem(SequencerContext* context, QGraphicsItem* parent)
+    : STimelineItem(context, parent)
+{
+    m_BoundingRect = QRectF(-1, 0, 2, Sequencer::SceneHeight);
+    setZValue(Sequencer::ZPlayheadItem);
+}
+
+void SRazorItem::SetX(int x)
+{
+    prepareGeometryChange();
+    setPos(x, 0.0);
+    update();
+}
+
+void SRazorItem::paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget)
+{
+    painter->setPen(QPen(option->palette.color(QPalette::Highlight), 1));
+    painter->drawLine(QPoint(0, 0), QPoint(0, boundingRect().height()));
+}
+
+/// STrackRazorItem
+
+STrackRazorItem::STrackRazorItem(SequencerContext* context, QGraphicsItem* parent)
+    : SRazorItem(context, parent)
+{
+    m_BoundingRect = QRectF(-1, 0, 2, Sequencer::TrackHeight);
+}
+
+VOID_NAMESPACE_CLOSE
---
+++ src/VoidUi/Sequencer/Graphics/SRazorItem.h
@@ -0,0 +1,32 @@
+// Copyright (c) 2025 waaake
+// Licensed under the MIT License
+
+#ifndef _SEQUENCER_RAZOR_ITEM_H
+#define _SEQUENCER_RAZOR_ITEM_H
+
+/* Internal */
+#include "STimelineItem.h"
+
+VOID_NAMESPACE_OPEN
+
+class SRazorItem : public STimelineItem
+{
+public:
+    explicit SRazorItem(SequencerContext* context, QGraphicsItem* parent = nullptr);
+    void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget) override;
+
+    void SetX(int x);
+};
+
+class STrackRazorItem : public SRazorItem
+{
+public:
+    explicit STrackRazorItem(SequencerContext* context, QGraphicsItem* parent = nullptr);
+    // void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget) override;
+
+    // void SetX(int x);
+};
+
+VOID_NAMESPACE_CLOSE
+
+#endif // _SEQUENCER_RAZOR_ITEM_H
