f68b15b0e1e83fed6b8d6fa763476ac7fa3dd74b
waaake <vivek_ve@outlook.com>
2026-08-07 10:31:03 +0530

    [VOID] Effect: Effect gets timeline_in and timeline_out values
    Also gets TrackItem as a member as to get what it's attached to at any given point

--- src/VoidObjects/Effects/Effects.cpp
+++ src/VoidObjects/Effects/Effects.cpp
@@ -7,9 +7,17 @@
 VOID_NAMESPACE_OPEN
 
 Effect::Effect(ImageOp* iop, const std::string& name, QObject* parent)
+    : Effect(iop, name, 0, 0, parent)
+{
+}
+
+Effect::Effect(ImageOp* iop, const std::string& name, v_frame_t in, v_frame_t out, QObject* parent)
     : VoidObject(parent)
     , m_Operator(iop)
     , m_Name(name)
+    , m_Color(225, 220, 225)
+    , m_TimelineIn(in)
+    , m_TimelineOut(out)
     , m_Enabled(true)
 {
 }
@@ -21,6 +29,24 @@ Effect::~Effect()
         delete m_Operator;
         m_Operator = nullptr;
     }
+}
+
+void Effect::SetTimelineItem(TrackItem* item)
+{
+    m_TrackItem = item;
+    emit updated();
+}
+
+void Effect::SetName(const std::string& name)
+{
+    m_Name = name;
+    emit updated();
+}
+
+void Effect::SetColor(const QColor& color)
+{
+    m_Color = color;
+    emit updated();
 }
 
 bool Effect::SetValue(const std::string& param, ValueType value)
@@ -61,6 +87,25 @@ void Effect::SetEnabled(bool enable)
 {
     m_Enabled = enable;
     emit updated();
+}
+
+void Effect::SetTimelineIn(v_frame_t frame)
+{
+    m_TimelineIn = frame;
+    emit rangeChanged(m_TimelineIn, m_TimelineOut);
+}
+
+void Effect::SetTimelineOut(v_frame_t frame)
+{
+    m_TimelineOut = frame;
+    emit rangeChanged(m_TimelineIn, m_TimelineOut);
+}
+
+void Effect::SetTimelineRange(v_frame_t start, v_frame_t end)
+{
+    m_TimelineIn = start;
+    m_TimelineOut = end;
+    emit rangeChanged(m_TimelineIn, m_TimelineOut);
 }
 
 VOID_NAMESPACE_CLOSE
