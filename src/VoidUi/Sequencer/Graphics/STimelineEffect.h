b3f8f858e9055cd10fd679d865ecbe4253b1bb83
waaake <vivek_ve@outlook.com>
2026-08-07 10:53:12 +0530

    [VOID] STimelineEffect: Added Visual representation of TimelineEffect in the sequencer

---
+++ src/VoidUi/Sequencer/Graphics/STimelineEffect.h
@@ -0,0 +1,36 @@
+// Copyright (c) 2025 waaake
+// Licensed under the MIT License
+
+#ifndef _SEQUENCER_TIMELINE_EFFECT_H
+#define _SEQUENCER_TIMELINE_EFFECT_H
+
+/* Internal */
+#include "Definition.h"
+#include "STimelineItem.h"
+
+VOID_NAMESPACE_OPEN
+
+class Effect;
+
+class STimelineEffect : public STimelineItem
+{
+public:
+    STimelineEffect(Effect* effect, SequencerContext* context, QGraphicsItem* parent = nullptr);
+
+    Effect* TimelineEffect() const { return m_Effect; }
+    void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget) override;
+    void Update() override;
+
+protected:
+    void mousePressEvent(QGraphicsSceneMouseEvent* event) override;
+
+private:
+    Effect* m_Effect;
+
+private: /* Methods */
+    void CalculateBoundingBox();
+};
+
+VOID_NAMESPACE_CLOSE
+
+#endif // _SEQUENCER_TIMELINE_EFFECT_H
