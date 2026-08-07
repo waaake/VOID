46358a0b254c2a7bf38578e7347317f7f655c344
waaake <vivek_ve@outlook.com>
2026-08-07 10:42:50 +0530

    [VOID] Sequence: emits maxTrackEffectsChanged on changes from track

--- src/VoidObjects/Sequence/Sequence.cpp
+++ src/VoidObjects/Sequence/Sequence.cpp
@@ -83,6 +83,7 @@ void PlaybackSequence::AddVideoTrack(const SharedPlaybackTrack& track)
     m_VideoTracks.push_back(track);
     connect(track.get(), &PlaybackTrack::rangeChanged, this, &PlaybackSequence::UpdateRange);
     connect(track.get(), &PlaybackTrack::updated, this, &PlaybackSequence::updated);
+    connect(track.get(), &PlaybackTrack::maxEffectsChanged, this, [=]() -> void { emit maxTrackEffectsChanged(track); });
 
     if (track->Name().empty())
     {
@@ -251,7 +252,7 @@ bool PlaybackSequence::HasMedia() const
 {
     /**
      * A sequence can be said empty if there are no tracks on it
-     * But the same seqeunce can have track(s) but no media in them
+     * But the same sequence can have track(s) but no media in them
      * This means that the sequence is not empty but has no media on it to be played
      */
     if (IsEmpty())    /* Already has no tracks on it */
