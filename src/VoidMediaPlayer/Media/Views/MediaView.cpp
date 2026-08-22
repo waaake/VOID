edcc4658fcdea854067b2837f3c4c904f56a3083
waaake <vivek_ve@outlook.com>
2026-08-22 10:03:29 +0000

    [VOID] MediaView: Reduce the pixmap size as currently hiding the pixmap is not implemented

--- src/VoidMediaPlayer/Media/Views/MediaView.cpp
+++ src/VoidMediaPlayer/Media/Views/MediaView.cpp
@@ -76,9 +76,9 @@ void MediaView::startDrag(Qt::DropActions supportedActions)
     QDrag* drag = new QDrag(this);
     drag->setMimeData(data);
 
-    /* Stacked pixmaps */
+    // Stacked pixmaps
     const int count = std::min(static_cast<int>(indexes.size()), 4);
-    const int thumbsize = 100;
+    const int thumbsize = 60;
     const int offset = 10;
 
     QSize canvas(thumbsize + offset * (count - 1), thumbsize + offset * (count - 1));
@@ -96,20 +96,6 @@ void MediaView::startDrag(Qt::DropActions supportedActions)
     painter.end();
 
     drag->setPixmap(stack);
-
-    connect(drag, &QDrag::targetChanged, this, [drag, stack](QObject* target) -> void
-    {
-        if (target)
-        {
-            QString name = target->objectName();
-            VOID_LOG_INFO("Target Changed... {0}", name.toStdString());
-            // if (dynamic_cast<STimelineView*>(target))
-            // {
-            //     VOID_LOG_INFO("Target is TimelineView");
-            // }
-        }
-    });
-
     drag->exec();
 }
 
