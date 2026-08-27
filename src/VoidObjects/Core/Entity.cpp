a2e0212e80db1d0fddefcb90ffdd9ee10a59b7c0
waaake <vivek_ve@outlook.com>
2026-08-27 10:53:29 +0530

    [VOID] Entity: Define the project Entity

---
+++ src/VoidObjects/Core/Entity.cpp
@@ -0,0 +1,34 @@
+// Copyright (c) 2025 waaake
+// Licensed under the MIT License
+
+/* Internal */
+#include "Entity.h"
+#include "VoidObjects/Project/Project.h"
+
+VOID_NAMESPACE_OPEN
+
+ProjectEntity::ProjectEntity(Core::Project* project)
+    : ProjectEntity(Qt::black, project)
+{
+}
+
+ProjectEntity::ProjectEntity(const QColor& color, Core::Project* project)
+    : VoidObject(project)
+    , m_Color(color)
+{
+}
+
+void ProjectEntity::SetColor(const QColor& color)
+{
+    m_Color = color;
+    emit updated();
+}
+
+QPixmap ProjectEntity::DefaultThumbnail() const
+{
+    QPixmap pix = QPixmap(QSize(400, 225));
+    pix.fill(Qt::black);
+    return pix;
+}
+
+VOID_NAMESPACE_CLOSE
