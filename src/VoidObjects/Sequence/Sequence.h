9a9394f1421f8acdc50a6212ac4704d6c00ca9b4
waaake <vivek_ve@outlook.com>
2026-09-06 20:26:11 +0530

    [VOID] Sequence: provides non-const access to the Snapshots for the model

--- src/VoidObjects/Sequence/Sequence.h
+++ src/VoidObjects/Sequence/Sequence.h
@@ -101,6 +101,7 @@ public:
     bool RestoreSnapshot(int index);
     int NumSnapshots() const { return static_cast<int>(m_Snapshots.size()); }
     const std::vector<Snapshot>& Snapshots() const { return m_Snapshots; }
+    std::vector<Snapshot>& Snapshots() { return m_Snapshots; }
 
     void Serialize(rapidjson::Value& out, rapidjson::Document::AllocatorType& allocator) const override;
     void Serialize(std::ostream& out) const override;
