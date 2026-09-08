db43d2f4b0de6d0bf333703b1f765744a0c7a2b8
waaake <vivek_ve@outlook.com>
2026-09-08 20:15:28 +0530

    [VOID] Tokens: Added ElementTokens
    ElementTokens are a way to store tokens within a given element name/path
    
    Right now it only tells the name and the version with room to grow in the future and is likely to be used when exporting within a project

---
+++ src/VoidCore/Media/Tokens.h
@@ -0,0 +1,42 @@
+// Copyright (c) 2025 waaake
+// Licensed under the MIT License
+
+#ifndef _MEDIA_TOKENS_H
+#define _MEDIA_TOKENS_H
+
+/* STD */
+#include <string>
+
+/* Internal */
+#include "Definition.h"
+
+VOID_NAMESPACE_OPEN
+
+struct VOID_API ElementTokens
+{
+    std::string name;
+    std::string version;
+    int vnum { 0 };
+
+    ElementTokens() {}
+    ElementTokens(const std::string& name)
+        : name(name) {}
+    ElementTokens(std::string&& name, std::string&& version)
+        : name(std::move(name)), version(std::move(version)) { ParseVersion(); }
+    ElementTokens(const std::string& name, const std::string& version)
+        : name(name), version(version) { ParseVersion(); }
+
+    bool HasVersion() const noexcept { return !version.empty(); }
+    bool Similar(const std::string& name) const noexcept { return name == this->name; }
+    bool Similar(const ElementTokens& other) const noexcept { return other.name == name; }
+
+    bool operator==(const ElementTokens& other) const noexcept { return other.name == name && other.vnum == vnum; }
+    bool operator!=(const ElementTokens& other) const noexcept { return other.vnum != vnum || other.name != name; }
+
+private:
+    void ParseVersion();
+};
+
+VOID_NAMESPACE_CLOSE
+
+#endif // _MEDIA_TOKENS_H
