d76fd0d2cc7eef34659f9b78fec9bcf222f1943a
waaake <vivek_ve@outlook.com>
2026-09-10 18:35:20 +0530

    [VOID] VersionSwitcher: Ensure to have the minimum height of 6 items if more versions exist

--- src/VoidSequencer/Widgets/SVersionSwitcher.cpp
+++ src/VoidSequencer/Widgets/SVersionSwitcher.cpp
@@ -47,7 +47,7 @@ void SVersionSwitcher::SetElementName(const QString& name)
 {
     m_View->SetElementName(name);
     int rows = m_View->RowCount();
-    setMaximumHeight((rows > 6 ? 360 : (rows * 60)) + 24);
+    setMinimumHeight((rows > 6 ? 360 : (rows * 60)) + 24);
 }
 
 void SVersionSwitcher::ResetMedia(const QModelIndex& index)
