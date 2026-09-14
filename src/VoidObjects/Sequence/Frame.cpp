ad1a3aeadc5c6cc05c4efd41b55728566022def3
waaake <vivek_ve@outlook.com>
2026-09-14 15:06:00 +0530

    [VOID] SequenceFrame: Holds Image for the frame
    Allows Image to be evaluated from the top level Effect set on the Frame

---
+++ src/VoidObjects/Sequence/Frame.cpp
@@ -0,0 +1,45 @@
+// Copyright (c) 2025 waaake
+// Licensed under the MIT License
+
+/* Internal */
+#include "Frame.h"
+#include "VoidObjects/Effects/Effects.h"
+#include "VoidCore/Logging.h"
+
+VOID_NAMESPACE_OPEN
+
+FloatImage SequenceFrame::Image()
+{
+    if (image->Empty() && frame)
+        frame->Image(image);
+
+    return image;
+}
+
+void SequenceFrame::Image(FloatImage& image)
+{
+    if (frame) frame->Image(image);
+}
+
+FloatImage SequenceFrame::Evaluate()
+{
+    // Evaluate and clean, unless the effect(s) are updated causing it to be dirty again...
+    if (effect && dirty)
+    {
+        /// TODO: This needs to be better organised and handled implicitly...
+        image->CreateEditable();
+
+        effect->Evaluate(image);
+        dirty = false;
+    }
+
+    return image;
+}
+
+void SequenceFrame::Clear()
+{
+    image->Clear();
+    dirty = true;
+}
+
+VOID_NAMESPACE_CLOSE
