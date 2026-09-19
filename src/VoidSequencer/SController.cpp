abc2f54534ef943fe0fab04ab99c5ea20c2466b3
waaake <vivek_ve@outlook.com>
2026-09-19 22:00:42 +0530

    [VOID] SequencerController: Added Methods allowing to reset the user defined in-out points on the timeline
    This is invoked through either a single item or a bunch of items

--- src/VoidSequencer/SController.cpp
+++ src/VoidSequencer/SController.cpp
@@ -493,6 +493,29 @@ void SequencerController::SwitchVersionExtremes(const std::unordered_set<SharedT
     stack->endMacro();
 }
 
+void SequencerController::ResetTimelineInOut(const SharedTrackItem& item)
+{
+    m_TimelineController->SetUserFirstframe(item->TimelineIn());
+    m_TimelineController->SetUserEndframe(item->TimelineOut());
+}
+
+void SequencerController::ResetTimelineInOut(const std::unordered_set<SharedTrackItem>& items)
+{
+    if (items.empty()) return;
+
+    const SharedTrackItem& first = *items.begin();
+    v_frame_t start = first->TimelineIn(), end = first->TimelineOut();
+
+    for (const SharedTrackItem& item : items)
+    {
+        start = std::min(item->TimelineIn(), start);
+        end = std::max(item->TimelineOut(), end);
+    }
+
+    m_TimelineController->SetUserFirstframe(start);
+    m_TimelineController->SetUserEndframe(end);    
+}
+
 STrack* SequencerController::TrackAt(int index) const
 {
     if (STimelineScene* scene = dynamic_cast<STimelineScene*>(m_Scene))
--- src/VoidSequencer/SController.h
+++ src/VoidSequencer/SController.h
@@ -72,6 +72,8 @@ public:
     void ScanVersions(const std::unordered_set<SharedTrackItem>& items);
     void SwitchVersion(const std::unordered_set<SharedTrackItem>& items, bool up);
     void SwitchVersionExtremes(const std::unordered_set<SharedTrackItem>& items, bool max);
+    void ResetTimelineInOut(const SharedTrackItem& item);
+    void ResetTimelineInOut(const std::unordered_set<SharedTrackItem>& items);
 
     STrack* TrackAt(const QPointF& position) const;
     STrack* TrackAt(int index) const;
